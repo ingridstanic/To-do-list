64e874e695b3ed614538c80cafb79f570dfb7e9d
Ingrid Stanic <ingridstanic@gmail.com>
2025-10-24 13:22:56 +0200

    created class Task and initialized list of standing items

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { Task } from "./models/Task";
 import "./style.css";
+
+const myBreaks = [
+    new Task("Break", "09:45", "10:00"),
+    new Task("Walk Albus the dumbledog", "12:00", "13:00"),
+    new Task("Eat something", "13:00", "13:15")
+]
+
+
+
---
+++ src/models/Task.js
@@ -0,0 +1,11 @@
+export class Task {
+    task;
+    startTime;
+    endTime;
+
+    constructor(task, startTime, endTime) {
+        this.task = task;
+        this.startTime = startTime;
+        this, endTime = endTime;
+    }
+}
